c4b283b9cfb43f3933ac4576d867a0d40aaa2edd
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2016-04-03 20:12:44 -0500

    adding dropdown menu to show the new option to add decorations

--- Home.php
+++ Home.php
@@ -109,7 +109,14 @@
 			<div id="sidebar-left" class="span2">
 				<div class="nav-collapse sidebar-nav">
 					<ul class="nav nav-tabs nav-stacked main-menu">
-                                            <li><a href="salon.php"><i class="icon-calendar"></i><span class="hidden-tablet">&nbsp;Salones</span></a></li>
+                                            <li>
+							<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span >Salones</span></a>
+							<ul>
+                                                            <li><a class="submenu" href="salon.php"><i class="icon-file-alt"></i><span class="hidden-tablet">Salones</span></a></li>
+								<li><a class="submenu" href="submenu2.html"><i class="icon-file-alt"></i><span class="hidden-tablet">Decoraciones</span></a></li>
+								
+							</ul>	
+						</li>
                                             <li><a href="events.php"><i class="icon-globe"></i><span class="hidden-tablet"> Eventos</span></a></li>
                                             <li><a href="services.php"><i class="icon-tags"></i><span class="hidden-tablet"> Servicios</span></a></li>
                                             <li><a href="images.php"><i class="icon-upload-alt"></i><span class="hidden-tablet">&nbsp;Subir Imagenes</span></a></li>
